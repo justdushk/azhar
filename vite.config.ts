35bb0b9aa94b55d98ba70098032cab1e1d52e0ba
justdushk <myasoedovjon92@gmail.com>
2025-12-10 14:06:33 +0500

    vrcl

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isVercel = process.env.VERCEL === '1'
+
 export default defineConfig({
-  base: '/azhar/',
+  base: isVercel ? '/' : '/azhar/',
   plugins: [react()],
 })
